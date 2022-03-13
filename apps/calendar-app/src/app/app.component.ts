18c8780f237aeb240f4cd98e7a64f40819c6e457
JustSergioPB <oigresanep@gmail.com>
2022-03-13 15:01:25 +0100

    Initial commit

---
+++ apps/calendar-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'uni-chain-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'calendar-app';
+}
