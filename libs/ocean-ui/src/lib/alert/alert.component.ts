89b85a4e1797e143cb69e368e51c1020006e6407
JustSergioPB <oigresanep@gmail.com>
2022-03-22 19:01:50 +0100

    master: OC Added other components

---
+++ libs/ocean-ui/src/lib/alert/alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'uni-chain-alert',
+  templateUrl: './alert.component.html',
+  styleUrls: ['./alert.component.scss']
+})
+export class AlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/ocean-ui/src/lib/ocean-ui.module.ts
+++ libs/ocean-ui/src/lib/ocean-ui.module.ts
@@ -2,12 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { TableComponent } from './table/table.component';
+import { SelectComponent } from './select/select.component';
+import { InputComponent } from './input/input.component';
+import { DatepickerComponent } from './datepicker/datepicker.component';
+import { AlertComponent } from './alert/alert.component';
+import { DialogComponent } from './dialog/dialog.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [
     SidebarComponent,
-    TableComponent
+    TableComponent,
+    SelectComponent,
+    InputComponent,
+    DatepickerComponent,
+    AlertComponent,
+    DialogComponent,
+    TabsComponent,
+    MenuComponent
   ],
 })
 export class OceanUiModule {}
