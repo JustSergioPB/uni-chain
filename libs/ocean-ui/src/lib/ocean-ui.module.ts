be2b0db50ee8a66333f889ae3517a146d5b402ff
JustSergioPB <oigresanep@gmail.com>
2022-03-22 18:51:48 +0100

    master: OC created sidebar and table components

--- libs/ocean-ui/src/lib/ocean-ui.module.ts
+++ libs/ocean-ui/src/lib/ocean-ui.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    SidebarComponent,
+    TableComponent
+  ],
 })
 export class OceanUiModule {}
